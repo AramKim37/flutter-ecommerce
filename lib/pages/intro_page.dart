c612de5af7b9a8b10930618048142d281e1f6790
AramKim37 <aramkim37@gmail.com>
2024-05-06 17:41:46 -0600

    add logo image and building intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -6,6 +7,69 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 25.0,
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  'lib/images/logo1.png',
+                  height: 400,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              //title
+              const Text(
+                "Just Do It",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Brand new sneakers and custom kicks made with premium quality",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Center(
+                      child: Text(
+                    "Shop Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
