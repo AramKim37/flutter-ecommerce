641e5df3d624dbad1fceb67f58cc403ae77e0ae0
AramKim37 <aramkim37@gmail.com>
2024-05-09 21:26:59 -0600

    add shop and cart pages. Also build route where user click the pages

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -3,23 +3,21 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-class MyBottomNavBar extends StatefulWidget {
-  const MyBottomNavBar({super.key});
+class MyBottomNavBar extends StatelessWidget {
+  void Function(int)? onTabChange;
+  MyBottomNavBar({super.key, required this.onTabChange});
 
-  @override
-  State<MyBottomNavBar> createState() => _MyBottomNavBarState();
-}
-
-class _MyBottomNavBarState extends State<MyBottomNavBar> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(vertical: 20),
       child: GNav(
         color: Colors.grey[400],
         activeColor: Colors.grey.shade700,
         tabActiveBorder: Border.all(color: Colors.white),
         tabBackgroundColor: Colors.grey.shade100,
         mainAxisAlignment: MainAxisAlignment.center,
+        onTabChange: (value) => onTabChange!(value),
         tabs: [
           GButton(
             icon: Icons.home,
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce/components/bottom_nav_bar.dart';
+import 'package:ecommerce/pages/cart_page.dart';
+import 'package:ecommerce/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -11,11 +13,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _seletedIndex = 0;
+  void navigateBottomBar(int index) {
+    setState(() {
+      _seletedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      bottomNavigationBar: MyBottomNavBar(),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_seletedIndex],
     );
   }
 }
